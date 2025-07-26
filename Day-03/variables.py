a90c2959d82672458a885b705f348516c0f88a38
ZoeInMyDna-code <zoeinmydna@gmail.com>
2025-07-26 01:40:26 +0100

    Worked On day 2 and 3 of Python

---
+++ Day-03/variables.py
@@ -0,0 +1,19 @@
+name = "Zoe"
+
+print(name);
+
+#Global Variables
+
+SS1a = 12  #Number of students
+SS1b = 10
+
+def SS1_total():
+    NewStudent = 3       #Local Variables
+    print(SS1a + SS1b + NewStudent);
+    
+def SS1_diff():
+    print(SS1a - SS1b)
+    
+SS1_total()
+
+SS1_diff()
